90efe277622bc8d5b6c80130dde0f6de45202bc5
Elmar Athmer <elmar@athmer.org>
2021-03-08 19:48:52 +0100

    extend the sun example to show rxjs capabilities

--- examples/sun.ts
+++ examples/sun.ts
@@ -1,17 +1,30 @@
 import { debugObserver, initHass } from "./index";
 import { hassDriver } from "../src/driver/hass";
-import { KnownEntities } from "../../hasso";
+import { KnownEntities } from "hasso";
+import { map, skip, take } from "rxjs/operators";
+import { concat, zip } from "rxjs";
+import { diffString } from "json-diff";
 
 /**
- * initialize driver and kick off paraluna (which wires together sources, sinks and main function)
+ * show usage of the hass source. Extended to match the "sun" example of hasso,
+ * which shows a colored diff
  */
-async function init() {
+async function sun() {
   const { hassToken, hassUrl } = initHass();
   const hass = await hassDriver(hassToken, hassUrl);
 
-  hass.source.state(KnownEntities.Sun).subscribe(debugObserver());
+  const sun$ = hass.source.state(KnownEntities.Sun);
+  const sunPrevious$ = sun$.pipe(skip(1));
+
+  // build a "sliding window" so we can always compare the current and the previous state
+  const sunDiff$ = zip(sun$, sunPrevious$).pipe(
+    map(([a, b]) => diffString(a, b)),
+  );
+
+  // prepend the initial state
+  return concat(sun$.pipe(take(1)), sunDiff$).subscribe(debugObserver());
 }
 
 if (require.main === module) {
-  init().catch((err) => console.error(err));
+  sun().catch((err) => console.error(err));
 }
