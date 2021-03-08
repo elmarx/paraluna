58059ae9e4d19cd85065336edc88f7821fb252f3
Elmar Athmer <elmar@athmer.org>
2021-03-08 22:22:42 +0100

    export types from commons

--- src/devices/index.ts
+++ src/devices/index.ts
@@ -1,7 +1,9 @@
-export * from "./ikea.trigger";
+export * from "./common";
+
 export * from "./ikea.light";
 export * from "./ikea.sensor";
+export * from "./ikea.trigger";
 
 export * from "./light";
-export * from "./light.source";
 export * from "./light.sink";
+export * from "./light.source";
