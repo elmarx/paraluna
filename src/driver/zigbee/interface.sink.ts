21fbb0f6578f09691330ccbd8cfb27853df072d1
Elmar Athmer <elmar@athmer.org>
2021-03-14 11:03:00 +0100

    support multiple devices in ZigbeePublish

--- src/driver/zigbee/interface.sink.ts
+++ src/driver/zigbee/interface.sink.ts
@@ -1,11 +1,23 @@
 import { Observable } from "rxjs";
 import { JsonValue } from "../../json";
 
-export type ZigbeePublish = {
+export type ZigbeeDevice = {
+  friendlyName: string;
+  attribute?: string;
+};
+
+export type ZigbeeSinglePublish = {
   friendlyName: string;
   attribute?: string;
   state: JsonValue;
 };
+
+export type ZigbeeMulitPublish = {
+  topic: string[] | ZigbeeDevice[];
+  state: JsonValue;
+};
+
+export type ZigbeePublish = ZigbeeSinglePublish | ZigbeeMulitPublish;
 
 // TODO: maybe also support Observable<ZigbeePublish>[] and/or Observable<ZigbeePublish[]>
 export type ZigbeeSink = (p: Observable<ZigbeePublish>) => void;
--- src/driver/zigbee/sink.ts
+++ src/driver/zigbee/sink.ts
@@ -1,27 +1,50 @@
 import { MqttMessage, MqttSink } from "../mqtt";
-import { map } from "rxjs/operators";
-import { ZIGBEE2MQTT_BASE_TOPIC, ZigbeePublish, ZigbeeSink } from "./interface";
+import { mergeMap } from "rxjs/operators";
+import {
+  ZIGBEE2MQTT_BASE_TOPIC,
+  ZigbeeDevice,
+  ZigbeePublish,
+  ZigbeeSink,
+} from "./interface";
+import { isZigbeeSinglePublish } from "./guards";
+import { JsonValue } from "../../json";
 
-/**
- * turn a zigbee publish command into an MqttMessage so it can be published by the mqttSink directly
- * @param v
- */
-function intoMqttMessage(v: ZigbeePublish): MqttMessage {
-  if (v.attribute) {
+function into(
+  state: JsonValue,
+  friendlyName: string,
+  attribute?: string,
+): MqttMessage {
+  if (attribute) {
     return {
-      topic: `${ZIGBEE2MQTT_BASE_TOPIC}/${v.friendlyName}/set/${v.attribute}`,
-      value: Buffer.from(JSON.stringify(v.state)),
+      topic: `${ZIGBEE2MQTT_BASE_TOPIC}/${friendlyName}/set/${attribute}`,
+      value: Buffer.from(JSON.stringify(state)),
     };
   } else {
     return {
-      topic: `${ZIGBEE2MQTT_BASE_TOPIC}/${v.friendlyName}/set`,
-      value: Buffer.from(JSON.stringify(v.state)),
+      topic: `${ZIGBEE2MQTT_BASE_TOPIC}/${friendlyName}/set`,
+      value: Buffer.from(JSON.stringify(state)),
     };
   }
 }
 
+/**
+ * turn a zigbee publish command into a MqttMessage(s) so it can be published by the mqttSink directly
+ * @param v
+ */
+function intoMqttMessage(v: ZigbeePublish): MqttMessage[] {
+  if (isZigbeeSinglePublish(v)) {
+    return [into(v.state, v.friendlyName, v.attribute)];
+  }
+
+  return v.topic.map((t: string | ZigbeeDevice) =>
+    typeof t === "string"
+      ? into(v.topic, t)
+      : into(v.state, t.friendlyName, t.attribute),
+  );
+}
+
 export function zigbeeSink(mqtt: MqttSink): ZigbeeSink {
   return (sink) => {
-    mqtt(sink.pipe(map(intoMqttMessage)));
+    mqtt(sink.pipe(mergeMap(intoMqttMessage)));
   };
 }
