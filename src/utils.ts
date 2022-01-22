7ca34fd2c2517887bd38d5f683834803e81f5e45
Elmar Athmer <elmar@athmer.org>
2022-01-22 19:30:50 +0100

    provide an unwrap util for Either

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { KnownColors, ZIGBEE2MQTT_COLORS } from "./devices";
+import { Either, isRight } from "fp-ts/Either";
 
 export function guessColor(x: number, y: number): KnownColors {
   return Object.entries(ZIGBEE2MQTT_COLORS)
@@ -10,4 +11,16 @@ export function guessColor(x: number, y: number): KnownColors {
       return { colorName, distance };
     })
     .sort((a, b) => a.distance - b.distance)[0]!.colorName as any;
+}
+
+/**
+ * unwrap an Either. This throws if ma is left, but returns the value otherwise
+ * @param ma
+ */
+export function unwrap<E, A>(ma: Either<E, A>): A {
+  if (isRight(ma)) {
+    return ma.right;
+  }
+
+  throw ma.left;
 }
