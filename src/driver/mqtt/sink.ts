e116a2a30af57b7dc1698d76c79053367ef33db5
Elmar Athmer <elmar@athmer.org>
2022-02-01 22:08:46 +0100

    fix types for most recent async-mqtt

--- src/driver/mqtt/sink.ts
+++ src/driver/mqtt/sink.ts
@@ -1,7 +1,7 @@
 import { Observable } from "rxjs";
 import { MqttMessage } from "./interface.message";
 import { Logger } from "winston";
-import { AsyncMqttClient, IPublishPacket } from "async-mqtt";
+import { AsyncMqttClient } from "async-mqtt";
 import { Either, isLeft } from "fp-ts/Either";
 import { tryCatch } from "fp-ts/TaskEither";
 
@@ -22,7 +22,7 @@ export function mqttSink(logger: Logger, client: AsyncMqttClient) {
       error(err: any): void {
         logger.error("MQTT Sink error: ", err);
       },
-      async next(value: MqttMessage): Promise<Either<unknown, IPublishPacket>> {
+      async next(value: MqttMessage): Promise<Either<unknown, void>> {
         const result = await tryCatch(
           () => client.publish(value.topic, value.value),
           (e) => e,
